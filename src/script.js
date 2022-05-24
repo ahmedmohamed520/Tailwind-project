e16244cfb9cc0d67f05b30fe6da91cfd3c12afae
ahmedmohamed520 <frontenddeveloper12020@gmail.com>
2022-05-24 20:39:26 +0200

    project files

---
+++ src/script.js
@@ -0,0 +1,6 @@
+"use strict";
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
+burger.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#ff6363",
+                secondary: {
+                    100: "#e2e2d5",
+                    200: "#888883",
+                },
+            },
+            fontFamily: {
+                body: ["Nunito"],
+            },
+        },
+    },
+    plugins: [],
+};
